57620a414841be82ec18655b5ff10c0b49fd1189
Siddhi <siddhic272@gmail.com>
2022-06-01 13:04:52 +0530

    Initialise commit

---
+++ subsequence1.py
@@ -0,0 +1,18 @@
+string1 = "srcte"
+string2 = "structures"
+
+def SubSequence(string1, string2, m, n):
+    if m == 0:
+        return True
+    if n == 0:
+        return False
+
+    if string1[m-1] == string2[n-1]:
+        return SubSequence(string1, string2, m-1, n-1)
+ 
+    return SubSequence(string1, string2, m, n-1)
+ 
+if SubSequence(string1, string2, len(string1), len(string2)):
+    print ("Valid Subsequence")
+else:
+    print ("Invalid Subsequence")
